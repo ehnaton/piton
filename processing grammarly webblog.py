c26a430d8e5fd9316ea51b36a43746473915d606
ehnaton <ehnaton4@gmail.com>
2014-10-11 11:29:12 +0300

    Update processing grammarly webblog.py

--- processing grammarly webblog.py
+++ processing grammarly webblog.py
@@ -1,5 +1,5 @@
 import os
-
+# running headless
 from pyvirtualdisplay import Display
 Display(visible=0, size=(1024, 768)).start()
 
@@ -21,13 +21,35 @@ blogs_file = 'blogs.json'
 blogs2_file = 'feeds_2.json'
 url_string = "http://www.grammarly.com/blog/page/{0}/"
 
+stop_words = nltk.corpus.stopwords.words('english') + [
+    '.',
+    ',',
+    '--',
+    '\'s',
+    '?',
+    ')',
+    '(',
+    ':',
+    '\'',
+    '\'re',
+    '"',
+    '-',
+    '}',
+    '{',
+    u'—',
+    '!',
+    u'•',
+    u'”',
+    u';'
+    ]
+
 def extract_blog_posts(url_string, PAGES = 48):
     blog_posts = []
     page_count = 0
     
     while(page_count<=PAGES):
         page_count+=1
-        url = url_string.format(page_count)
+        url = url_string.format(page_count) # create url
         driver.get(url)
         
         try:        
@@ -51,8 +73,6 @@ def extract_blog_posts(url_string, PAGES = 48):
             blog_posts.append({'title': e.text, 'content': clean_html(texts), 'link': e.get_attribute('href')})
             return blog_posts
 
-blog_posts = extract_blog_posts(url_string)  
-
 def save_blog_posts_to(file_name, blog_posts):
     #save to a dict
     out_file = os.path.join(shared_folder, file_name)
@@ -61,6 +81,20 @@ def save_blog_posts_to(file_name, blog_posts):
     f.close()
     print 'Wrote to %s' % ( f.name, )
 
+def words_distribution_of_text(BLOG_DATA):    
+    posts = json.loads(open(BLOG_DATA).read())
+    texts = u",".join([post_block['content'] for post_block in posts])
+    
+    sentences = nltk.tokenize.sent_tokenize(texts)
+    
+    words = [w.lower() for sentence in sentences for w in
+             nltk.tokenize.word_tokenize(sentence)]
+    
+    fdist = nltk.FreqDist(words)
+    return (fdist,words, texts)
+
+# main functions call
+blog_posts = extract_blog_posts(url_string)  
 save_blog_posts_to(shared_folder, blogs_file)
 
 #save to a list
@@ -73,55 +107,22 @@ f = open(out_file, 'w')
 f.write(json.dumps(list_blogs, indent=1))
 f.close()
 print 'Wrote to %s' % ( f.name, )
-blog_data = json.loads(open(os.path.join(shared_folder,blogs_file).read())
 
-# Customize the list of stopwords, add common
-# punctuation and contraction artifacts.
+#words distribution
+fdist, words, texts = words_distribution_of_text(BLOG_DATA)
+num_words = sum([i[1] for i in fdist.items()])
+num_unique_words = len(fdist.keys())
 
-stop_words = nltk.corpus.stopwords.words('english') + [
-    '.',
-    ',',
-    '--',
-    '\'s',
-    '?',
-    ')',
-    '(',
-    ':',
-    '\'',
-    '\'re',
-    '"',
-    '-',
-    '}',
-    '{',
-    u'—',
-    ]
+# Hapaxes are words that appear only once
+num_hapaxes = len(fdist.hapaxes())
 
-for post in blog_data:
-    sentences = nltk.tokenize.sent_tokenize(post['content'])
+top_10_words_sans_stop_words = [w for w in fdist.items() if w[0]
+                                not in stop_words][:20]
 
-    words = [w.lower() for sentence in sentences for w in
-             nltk.tokenize.word_tokenize(sentence)]
-
-    fdist = nltk.FreqDist(words)
-
-    # Basic stats
-
-    num_words = sum([i[1] for i in fdist.items()])
-    num_unique_words = len(fdist.keys())
-
-    # Hapaxes are words that appear only once
-
-    num_hapaxes = len(fdist.hapaxes())
-
-    top_10_words_sans_stop_words = [w for w in fdist.items() if w[0]
-                                    not in stop_words][:10]
-
-    print post['title']
-    print '\tNum Sentences:'.ljust(25), len(sentences)
-    print '\tNum Words:'.ljust(25), num_words
-    print '\tNum Unique Words:'.ljust(25), num_unique_words
-    print '\tNum Hapaxes:'.ljust(25), num_hapaxes
-    print '\tTop 10 Most Frequent Words (sans stop words):\n\t\t', \
-            '\n\t\t'.join(['%s (%s)'
-            % (w[0], w[1]) for w in top_10_words_sans_stop_words])
-    print
+print '\tNum Words:'.ljust(25), num_words
+print '\tNum Unique Words:'.ljust(25), num_unique_words
+print '\tNum Hapaxes:'.ljust(25), num_hapaxes
+print '\tTop 10 Most Frequent Words (sans stop words):\n\t\t', \
+        '\n\t\t'.join(['%s (%s)'
+        % (w[0], w[1]) for w in top_10_words_sans_stop_words])
+print
